b801658b1e1583f7a29bf94439058d5e6ca054f1
Eugene-44hub <eugeneishado@gmail.com>
2022-07-23 12:36:15 +0100

    footer

--- src/components/learn/Learn.tsx
+++ src/components/learn/Learn.tsx
@@ -11,11 +11,11 @@ const Learn = () => {
           Learn About Cryptocurrency
         </Header>
         <p className='text-center text-white mt-[16px] mb-[43px] md:mb-[66px]'>Learn about crypto currency to start investing</p>
-        <div className='lg:flex lg:flex-wrap justify-between mt-5'>
+        <div className='lg:flex flex-wrap justify-between mt-5'>
           <img className=' lg:mx-0 lg:mt-0 lg:w-[48%] mb-5 md:w-[40%] sm:w-[50%] w-[70%]  mx-auto' src={learn} alt="news" />
           {Array(6).fill(null).map((item, index) => (<Card />))}
         </div>
-        <div className='lg:text-start text-center '>
+        <div className='lg:text-start mb-5 text-center '>
         <button className={`${styles.marketsBtn}`}>See All Markets</button>
         </div>
       </article>
